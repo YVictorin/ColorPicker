ebc5a15e8a33d709e7cc0270c96ac06c23ddb271
YVictorin <vicyonjou@gmail.com>
2025-02-04 14:06:02 -0500

    feat: add PropTypes for ColorDisplay component

--- src/components/ColorDisplay.jsx
+++ src/components/ColorDisplay.jsx
@@ -1,5 +1,11 @@
+import PropTypes from "prop-types"
+
 export default function ColorDisplay({ color }) {
     return (
         <div className="color-display" style={{ backgroundColor: color }}></div>
     )
+}
+
+ColorDisplay.PropTypes = {
+    color: PropTypes.string
 }
